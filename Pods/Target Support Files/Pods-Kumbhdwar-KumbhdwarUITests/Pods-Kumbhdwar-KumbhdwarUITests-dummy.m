9ef766042df78a67d4e7f9c863390fb167f19964
pramod yadav <pramod.yadav@mdoc.one>
2021-03-13 10:28:36 +0530

    initial commit

---
+++ Pods/Target Support Files/Pods-Kumbhdwar-KumbhdwarUITests/Pods-Kumbhdwar-KumbhdwarUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Kumbhdwar_KumbhdwarUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Kumbhdwar_KumbhdwarUITests
+@end
---
+++ Pods/Target Support Files/Pods-KumbhdwarTests/Pods-KumbhdwarTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KumbhdwarTests : NSObject
+@end
+@implementation PodsDummy_Pods_KumbhdwarTests
+@end
